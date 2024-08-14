888659f32576a62b3440f860c1f9333895d30fc6
Mauxd777 <103484354+Mauxd777@users.noreply.github.com>
2024-08-13 18:21:17 -0600

    1

--- AutoMax/routing.php
+++ AutoMax/routing.php
@@ -24,68 +24,8 @@ if ($_GET['page'] == 'inicio') {
 }
 
 
-
-
 if ($_GET['page'] == 'tablas') {
 	require_once('views/tablas.php');
-}
-
-if ($_GET['page'] == 'vehiculos') {
-	require_once('views/vehiculos.php');
-}
-
-if ($_GET['page'] == 'marcas') {
-	require_once('views/marcas.php');
-}
-
-if ($_GET['page'] == 'modelos') {
-	require_once('views/modelos.php');
-}
-
-if ($_GET['page'] == 'repuestos') {
-	require_once('views/repuestos.php');
-}
-
-if ($_GET['page'] == 'talleres') {
-	require_once('views/talleres.php');
-}
-
-if ($_GET['page'] == 'oficinas') {
-	require_once('views/oficinas.php');
-}
-
-if ($_GET['page'] == 'clientes') {
-	require_once('views/clientes.php');
-}
-
-if ($_GET['page'] == 'proveedores') {
-	require_once('views/proveedores.php');
-}
-
-if ($_GET['page'] == 'facturas') {
-	require_once('views/facturas.php');
-}
-
-if ($_GET['page'] == 'servicios') {
-	require_once('views/servicios.php');
-}
-
-if ($_GET['page'] == 'detalle_factura') {
-	require_once('views/detalleFactura.php');
-}
-
-if ($_GET['page'] == 'estados_vehiculos') {
-	require_once('views/estadosVehiculos.php');
-}
-
-if ($_GET['page'] == 'tipos_vehiculos') {
-	require_once('views/tiposVehiculos.php');
-}
-if ($_GET['page'] == 'VISTA_DETALLE_FACTURA') {
-	require_once('views/VISTA_DETALLE_FACTURA.php');
-}
-if ($_GET['page'] == 'VISTA_FACTURAS') {
-	require_once('views/vistaFacturas.php');
 }
 
 
